e094a3e60e72e702d6d409553cc47cd3a5507132
lucapolzone <lucapolzone@gmail.com>
2024-02-27 07:03:50 +0100

    feat: add starting files

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+  require_once "./main.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="author" content="Luca Polzone" />
+  <meta name="description" content="Esercizio - PHP Hotel" />
+  <title>Esercizio - PHP Hotel</title>
+  <!-- Bootstrap link -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+  
+
+</body>
+</html>
